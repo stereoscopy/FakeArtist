1fd32ab6deb134c9a447863c6f6974d4deb3f1b4
Neill Bogie <neillbogie@googlemail.com>
2016-05-10 19:37:46 +0100

    done prototype of taking player names

--- js/game.js
+++ js/game.js
@@ -1,3 +1,24 @@
+$(document).ready(function(){
+  //$('#header ul').addClass('hide');
+  //$('#header').append('<div class="leftButton" onclick="toggleMenu()">Menu</div>');
+
+  $('#addPlayerButton').bind('click', addPlayer);
+  $('#distributeTopicButton').bind('click', buildScreen2);
+  addPlayer({ name: "Alice" } );
+  addPlayer({ name: "Bob" } );
+  addPlayer({ name: "Charlie" } );
+});
+
+function toggleMenu() {
+  $('#header ul').toggleClass('hide');
+  $('#header .leftButton').toggleClass('pressed');
+}
+
+function getPlayersFromForm(){
+  $('.playerNameInput')
+}
+
+
 function assert(message, expr) {
   if (!expr) {
     throw new Error(message);
@@ -8,8 +29,18 @@ function assert(message, expr) {
 
 assert.count = 0;
 
+function deletePlayer(foo){
+  console.log('deleting player' + foo);
+  $(foo).parent().remove();
+}
 
+function addPlayer(opts){
+  var numPlayers = $('#playerList li').size();
+  var n = opts.name || ("Player" + (numPlayers + 1));
+  $("<li><input type='text' class='playerNameInput' value='"+n+"'/><span class='deletePlayer' onclick='deletePlayer(this);'>(delete)</span></li>").appendTo('#playerList');
+}
 
-function init(){
-
+function buildScreen2(){
+  console.log('building screen 2');
+  var playerLIs = $('#playerList li').size();
 }
